c2d8699005347746c4c6ea27032628d123e49bbe
Jaxc <cooljaxc@gmail.com>
2017-10-08 17:19:43 +0200

    inital commit

---
+++ Inc/main.h
@@ -0,0 +1,121 @@
+/**
+  ******************************************************************************
+  * File Name          : main.h
+  * Description        : This file contains the common defines of the application
+  ******************************************************************************
+  ** This notice applies to any and all portions of this file
+  * that are not between comment pairs USER CODE BEGIN and
+  * USER CODE END. Other portions of this file, whether 
+  * inserted by the user or by software development tools
+  * are owned by their respective copyright owners.
+  *
+  * COPYRIGHT(c) 2017 STMicroelectronics
+  *
+  * Redistribution and use in source and binary forms, with or without modification,
+  * are permitted provided that the following conditions are met:
+  *   1. Redistributions of source code must retain the above copyright notice,
+  *      this list of conditions and the following disclaimer.
+  *   2. Redistributions in binary form must reproduce the above copyright notice,
+  *      this list of conditions and the following disclaimer in the documentation
+  *      and/or other materials provided with the distribution.
+  *   3. Neither the name of STMicroelectronics nor the names of its contributors
+  *      may be used to endorse or promote products derived from this software
+  *      without specific prior written permission.
+  *
+  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  *
+  ******************************************************************************
+  */
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+  /* Includes ------------------------------------------------------------------*/
+
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Private define ------------------------------------------------------------*/
+
+#define D_Pin GPIO_PIN_2
+#define D_GPIO_Port GPIOE
+#define Eb_Pin GPIO_PIN_3
+#define Eb_GPIO_Port GPIOE
+#define E_Pin GPIO_PIN_4
+#define E_GPIO_Port GPIOE
+#define F_Pin GPIO_PIN_5
+#define F_GPIO_Port GPIOE
+#define Gb_Pin GPIO_PIN_6
+#define Gb_GPIO_Port GPIOE
+#define OTG_FS_PowerSwitchOn_Pin GPIO_PIN_0
+#define OTG_FS_PowerSwitchOn_GPIO_Port GPIOC
+#define B1_Pin GPIO_PIN_0
+#define B1_GPIO_Port GPIOA
+#define BOOT1_Pin GPIO_PIN_2
+#define BOOT1_GPIO_Port GPIOB
+#define G_Pin GPIO_PIN_7
+#define G_GPIO_Port GPIOE
+#define Ab_Pin GPIO_PIN_8
+#define Ab_GPIO_Port GPIOE
+#define A_Pin GPIO_PIN_9
+#define A_GPIO_Port GPIOE
+#define Bb_Pin GPIO_PIN_10
+#define Bb_GPIO_Port GPIOE
+#define B_Pin GPIO_PIN_11
+#define B_GPIO_Port GPIOE
+#define c_Pin GPIO_PIN_12
+#define c_GPIO_Port GPIOE
+#define db_Pin GPIO_PIN_13
+#define db_GPIO_Port GPIOE
+#define d_Pin GPIO_PIN_14
+#define d_GPIO_Port GPIOE
+#define eb_Pin GPIO_PIN_15
+#define eb_GPIO_Port GPIOE
+#define LD4_Pin GPIO_PIN_12
+#define LD4_GPIO_Port GPIOD
+#define LD3_Pin GPIO_PIN_13
+#define LD3_GPIO_Port GPIOD
+#define LD5_Pin GPIO_PIN_14
+#define LD5_GPIO_Port GPIOD
+#define LD6_Pin GPIO_PIN_15
+#define LD6_GPIO_Port GPIOD
+#define MIDI_OUT_Pin GPIO_PIN_9
+#define MIDI_OUT_GPIO_Port GPIOA
+#define MIDI_IN_Pin GPIO_PIN_10
+#define MIDI_IN_GPIO_Port GPIOA
+#define Audio_RST_Pin GPIO_PIN_4
+#define Audio_RST_GPIO_Port GPIOD
+#define OTG_FS_OverCurrent_Pin GPIO_PIN_5
+#define OTG_FS_OverCurrent_GPIO_Port GPIOD
+#define C_Pin GPIO_PIN_0
+#define C_GPIO_Port GPIOE
+#define Db_Pin GPIO_PIN_1
+#define Db_GPIO_Port GPIOE
+
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+void _Error_Handler(char *, int);
+
+#define Error_Handler() _Error_Handler(__FILE__, __LINE__)
+
+/**
+  * @}
+  */ 
+
+/**
+  * @}
+*/ 
+
+#endif /* __MAIN_H */
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
