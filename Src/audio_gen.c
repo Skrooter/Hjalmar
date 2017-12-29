fa5daa70c0e612f52406f720443432889f107e55
Jaxc <cooljaxc@gmail.com>
2017-12-29 14:15:13 +0100

    All Wave forms are now functional
    Added Triangle wave

--- Src/audio_gen.c
+++ Src/audio_gen.c
@@ -24,13 +24,13 @@ uint32_t sample_position;
 int16_t last_value;
 int16_t SQ_MAX = 0;
 
-int pulse_pos = 2;
+int pulse_pos = 15;
 
 void audio_gen_sqr_start(float freq, uint8_t level)
 {
     I2S_HandleTypeDef *i2s_handle = get_i2s_handle();
     sample_per_sec = i2s_handle->Init.AudioFreq;
-    wave_type = WAVE_SQUARE;
+    wave_type = WAVE_TRIANGLE;
     wave_period = 44; //sample_per_sec / freq;
     sample_position = 0;
     last_value = (INT16_MIN / (128 - level))+1;
@@ -61,20 +61,17 @@ void fetch_next_audio_buffer(uint16_t *audio_samples, uint16_t n_sample)
     case WAVE_SAW:
         for(int i = 0; i < n_sample; i++) {
             if (sample_position >= 0xFFFF){
-                sample_position = 0;
+                sample_position = 0x0000;
             }
             else{
-                sample_position++;
+                sample_position += 0xFFFF / wave_period;
             }
-            audio_samples[i] = (int16_t)sample_position;
+            audio_samples[i] = sample_position - 0x7FFF;
         }
         break;
 
     case WAVE_PULSE:
         for(int i = 0; i < n_sample; i++) {
-            if(i % 2 == 0) {
-                continue;
-            }
             if (sample_position >= wave_period) {
                 sample_position = 0;
             }
@@ -82,11 +79,11 @@ void fetch_next_audio_buffer(uint16_t *audio_samples, uint16_t n_sample)
                 sample_position++;
             }
 
-            if (i == pulse_pos) {
-                audio_samples[i] = 0xF;
+            if (i >= pulse_pos) {
+                audio_samples[i] = 0x7FFF;
             }
             else {
-                audio_samples[i] = 0;
+                audio_samples[i] = 0x8000;
             }
         }
         break;
@@ -99,10 +96,28 @@ void fetch_next_audio_buffer(uint16_t *audio_samples, uint16_t n_sample)
             else{
                 sample_position++;
             }
-
-            audio_samples[i] = (int16_t) 0xFF * sin(2 * M_PI * sample_position / wave_period);
+            double gen_sin = 0x7FFF * sin(2 * M_PI * sample_position / wave_period);
+            audio_samples[i] = (int16_t) gen_sin;
         }
         break;
+
+    case WAVE_TRIANGLE:
+        for(int i = 0; i < n_sample; i++) {
+
+            sample_position += 0xFFFF / wave_period;
+            if (sample_position >= 2 * 0xFFFF){
+                sample_position = 0x0000;
+                audio_samples[i] = 0x8000;
+            }
+            else if (sample_position < 0xFFFF) {
+                audio_samples[i] = sample_position - 0x7FFF;
+            }
+            else {
+                audio_samples[i] = (0xFFFF + 0x7FFF) - sample_position;
+            }
+
+        }
+        break;;
 
     default:
         break;
